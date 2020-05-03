bf48b050266ef1227fbad08635c68269a6698898
vlabster <vlabster@mail.ru>
2020-05-03 11:55:53 +0300

    added db config, modules struct

---
+++ modules/theme.module.php
@@ -0,0 +1,17 @@
+<?
+class ThemeModule {
+    
+    private $pdo;
+    
+    function __construct()
+    {
+        $this->initDb();
+    }
+
+    function initDb()
+    {
+        include 'db.php';
+        $pdo = new PDO($_dsn, $_user, $_pass, $_opt);
+        $this->pdo = $pdo;
+    }
+}
---
+++ modules/user.module.php
@@ -0,0 +1,17 @@
+<?
+class UserModule {
+
+    private $pdo;
+    
+    function __construct()
+    {
+        $this->initDb();
+    }
+
+    function initDb()
+    {
+        include 'db.php';
+        $pdo = new PDO($_dsn, $_user, $_pass, $_opt);
+        $this->pdo = $pdo;
+    }
+}
