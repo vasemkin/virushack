7427f286e42a204cb1117ff95fed1be4df982310
vlabster <vlabster@mail.ru>
2020-05-03 17:06:13 +0300

    /

---
+++ modules/templates/courses/filters.tpl.php
@@ -0,0 +1,52 @@
+<div class="courses__right">
+    <div class="courses__options">
+        <div class="courses__title">Фильтры</div>
+        <form action="" class="courses__area">
+
+        </form>
+        <div class="courses__title courses__title_blue">Область</div>
+
+        <label class="container-checkbox">Технические науки
+            <input type="checkbox" checked="checked">
+            <span class="checkmark"></span>
+        </label>
+        <label class="container-checkbox">Гуманитарные науки
+            <input type="checkbox">
+            <span class="checkmark"></span>
+        </label>
+        <label class="container-checkbox">Естественные науки
+            <input type="checkbox">
+            <span class="checkmark"></span>
+        </label>
+
+
+        <div class="courses__title courses__title_blue">Категории</div>
+
+        <label class="container-checkbox">Поп курсы
+            <input type="checkbox" checked="checked">
+            <span class="checkmark"></span>
+        </label>
+        <label class="container-checkbox">Новые курсы
+            <input type="checkbox">
+            <span class="checkmark"></span>
+        </label>
+        <label class="container-checkbox">Для школьников
+            <input type="checkbox">
+            <span class="checkmark"></span>
+        </label>
+        <label class="container-checkbox">Для студентов
+            <input type="checkbox">
+            <span class="checkmark"></span>
+        </label>
+        <label class="container-checkbox">Для всех
+            <input type="checkbox">
+            <span class="checkmark"></span>
+        </label>
+
+    </div><!-- /courses__options -->
+
+    <div class="courses__pic">
+        <img width="168px" height="126" class="courses__image" src="img/courses__image.png" alt="courses">
+    </div>
+
+</div>
